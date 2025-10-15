2cc1f2c2d169c404a03be5261f1be2ee52970c56
Jenna <jli33@academic.rrc.ca>
2025-10-15 11:04:22 -0500

    Defined 2 one-to-many model associations.

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,4 @@
 class Breed < ApplicationRecord
+  has_many :favourites
+  has_many :users, through: :favourites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :favourites
+  has_many :breeds, through: :favourites
 end
