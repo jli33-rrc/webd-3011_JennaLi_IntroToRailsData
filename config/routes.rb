702d0c2a5ee3af07dbec1019909c5c569f913781
Jenna <jli33@academic.rrc.ca>
2025-10-16 10:31:26 -0500

    Generated a controller and view for Users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
   get "breeds/index"
   get "breeds/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
